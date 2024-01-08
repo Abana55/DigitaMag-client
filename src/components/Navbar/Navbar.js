22cd44dc3028f5e83c9958587bffec85648739a6
Alec bana <anbana994@gmail.com>
2024-01-08 18:14:29 -0500

    added navbar code

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* Navbar content */}
+        </nav>
+    );
+};
+
+export default Navbar;
