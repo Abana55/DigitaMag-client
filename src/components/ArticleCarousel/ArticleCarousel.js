012fec75b4507e1082bc21cfa1a385366f105253
Alec bana <anbana994@gmail.com>
2024-01-16 22:03:38 -0500

    basic's added to the carousel compnonent

---
+++ src/components/ArticleCarousel/ArticleCarousel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ArticleCarousel.scss';
+
+const ArticleCarousel = ({ articles }) => {
+    // Carousel implementation
+    return <div>{/* Carousel JSX */}</div>;
+};
+
+export default ArticleCarousel;
